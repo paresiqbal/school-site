362f3dac20f07459973fa5ad286f7ea26a357941
paresiqbal <thecrowdel@gmail.com>
2024-09-19 15:15:42 +0700

    Refactor layout.tsx to use ClientLayout component for wrapping the app with session and app context providers

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,28 @@
+import { AppContext } from "@/context/AppContext";
+import Image from "next/image";
+import { useContext } from "react";
+
 export default function Dashboard() {
+  const { user } = useContext(AppContext);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Wellcome Back !</h1>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+
+        {user && (
+          <div>
+            <p>Welcome, {user.name}!</p>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
