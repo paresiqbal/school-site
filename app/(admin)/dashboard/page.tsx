eed5cea92c1682e5cc2d44e3eee6374b578ce637
paresiqbal <thecrowdel@gmail.com>
2024-09-20 00:00:04 +0700

    Refactor Dashboard page to use next-auth/react for session management and display user name from session

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { AppContext } from "@/context/AppContext";
-import { signOut } from "next-auth/react";
-import { useContext } from "react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Dashboard() {
-  const { user } = useContext(AppContext);
-
-  console.log(user);
+  // Get session from next-auth
+  const { data: session } = useSession();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {user && (
+        {session?.user && (
           <div>
-            <p className="text-white">Welcome, {user.name}!</p>
+            <p className="text-white">Welcome, {session.user.name}!</p>
           </div>
         )}
       </main>
