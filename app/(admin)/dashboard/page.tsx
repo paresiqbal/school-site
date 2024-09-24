78fcd3e80c9061e6e9bc5fdec0726af530f16c05
paresiqbal <thecrowdel@gmail.com>
2024-09-24 18:32:00 +0700

    Refactor layout component to add full-width background to dashboard content

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -2,11 +2,30 @@
 
 import { useSession } from "next-auth/react";
 
+// ui lib
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
 export default function Dashboard() {
   const { data: session } = useSession();
 
   return (
     <div>
+      <Breadcrumb className="hidden md:flex pb-4">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbPage className="font-bold text-white/70 hover:text-white">
+              Recent Orders
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+        </BreadcrumbList>
+      </Breadcrumb>
       {session?.user && (
         <div>
           <h1 className="font-bold text-2xl">Welcome, {session.user.name}!</h1>
