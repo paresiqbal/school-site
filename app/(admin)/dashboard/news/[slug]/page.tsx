9da48f3e334e0b7e4817d7352a2952e3d5196824
paresiqbal <thecrowdel@gmail.com>
2024-10-01 23:02:46 +0700

    Refactor CreateNews component to improve image handling, file structure, and add image upload functionality

--- app/(admin)/dashboard/news/[slug]/page.tsx
+++ app/(admin)/dashboard/news/[slug]/page.tsx
@@ -5,7 +5,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { AppContext } from "@/context/AppContext";
 
-// ui lib
+// UI library imports
+import { Toaster, toast } from "sonner";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -14,11 +22,25 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Toaster, toast } from "sonner";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
-// icons
+// Icons
 import { Loader } from "lucide-react";
+
+// Zod for validation
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 type DetailNewsProps = { params: { slug: string } };
 
@@ -30,13 +52,29 @@ interface NewsDetail {
   created_at: string;
 }
 
+const formSchema = z.object({
+  title: z.string().min(3, { message: "Title must be at least 3 characters." }),
+  content: z
+    .string()
+    .min(10, { message: "Content must be at least 10 characters." }),
+  image: z.any().optional(),
+});
+
 export default function EditNews({ params }: DetailNewsProps) {
   const { token } = useContext(AppContext);
   const [newsDetail, setNewsDetail] = useState<NewsDetail | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [editedTitle, setEditedTitle] = useState("");
-  const [editedContent, setEditedContent] = useState("");
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      image: "",
+    },
+  });
 
   useEffect(() => {
     async function fetchNewsDetail() {
@@ -58,8 +96,8 @@ export default function EditNews({ params }: DetailNewsProps) {
 
         const data = await res.json();
         setNewsDetail(data);
-        setEditedTitle(data.title);
-        setEditedContent(data.content);
+        form.setValue("title", data.title);
+        form.setValue("content", data.content);
         toast.success("News loaded successfully");
       } catch (error) {
         console.error(error);
@@ -73,8 +111,13 @@ export default function EditNews({ params }: DetailNewsProps) {
     fetchNewsDetail();
   }, [params.slug, token]);
 
-  const handleSave = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedImage(event.target.files[0]);
+    }
+  };
+
+  const handleSave = async (data: z.infer<typeof formSchema>) => {
     try {
       const res = await fetch(
         `http://127.0.0.1:8000/api/news/${newsDetail?.id}`,
@@ -85,8 +128,8 @@ export default function EditNews({ params }: DetailNewsProps) {
             Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({
-            title: editedTitle,
-            content: editedContent,
+            title: data.title,
+            content: data.content,
           }),
         }
       );
@@ -135,42 +178,89 @@ export default function EditNews({ params }: DetailNewsProps) {
 
       <Card className="mb-6 p-4">
         <CardHeader>
-          <h2 className="text-2xl md:text-3xl mb-2">Edit News</h2>
-          <form onSubmit={handleSave}>
-            <input
-              type="text"
-              value={editedTitle}
-              onChange={(e) => setEditedTitle(e.target.value)}
-              className="text-2xl md:text-3xl w-full mb-2"
-              required
-            />
-            {newsDetail.image && (
-              <div className="w-full mb-4">
-                <Image
-                  src={`http://localhost:8000/${newsDetail.image}`}
-                  alt={newsDetail.title}
-                  width={600}
-                  height={400}
-                  className="rounded-lg h-auto object-cover"
+          <CardTitle>Edit News</CardTitle>
+          <CardDescription>
+            Change this form to update the news.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="mt-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSave)}>
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="News title"
+                        {...field}
+                        className="w-full rounded-lg"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {newsDetail.image && (
+                <div className="w-full mb-4">
+                  <Image
+                    src={`http://localhost:8000/${newsDetail.image}`}
+                    alt={newsDetail.title}
+                    width={600}
+                    height={400}
+                    className="rounded-lg h-auto object-cover"
+                  />
+                </div>
+              )}
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Content</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        rows={5}
+                        placeholder="News content"
+                        {...field}
+                        className="w-full"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="relative">
+                <label
+                  htmlFor="fileInput"
+                  className="absolute left-4 bottom-4 cursor-pointer"
+                >
+                  <span className="text-gray-500">Attach Image</span>
+                </label>
+                <input
+                  id="fileInput"
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleFileChange}
                 />
               </div>
-            )}
-            <textarea
-              value={editedContent}
-              onChange={(e) => setEditedContent(e.target.value)}
-              rows={5}
-              className="w-full mb-4"
-              required
-            />
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              Save
-            </button>
-          </form>
-        </CardHeader>
-        <CardContent className="mt-4"></CardContent>
+              {selectedImage && (
+                <p className="mt-2 text-sm text-gray-600">
+                  Selected file: {selectedImage.name}
+                </p>
+              )}
+              <Button
+                type="submit"
+                className="font-bold w-full p-2 rounded mt-4"
+              >
+                Save
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   );
