28990c5754e1978cf5b56087d8425f3efe4e2d52
paresiqbal <thecrowdel@gmail.com>
2024-10-09 14:51:02 +0700

    Refactor CreateAgenda page: Remove unused code for date picker state

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,35 +1,22 @@
 "use client";
 
+import CalendarComps from "@/components/Calendar";
 import { useSession } from "next-auth/react";
 
 // ui lib
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 
 export default function Dashboard() {
   const { data: session } = useSession();
 
   return (
     <div>
-      <Breadcrumb className="hidden md:flex pb-4">
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbPage>Dashboard</BreadcrumbPage>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-        </BreadcrumbList>
-      </Breadcrumb>
       {session?.user && (
-        <div>
+        <>
           <h1 className="font-bold text-2xl">
             Welcome, <span className="text-primary">{session.user.name}!</span>
           </h1>
-        </div>
+          <CalendarComps />
+        </>
       )}
     </div>
   );
---
+++ components/Calendar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+// ex lib
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
+
+// ui lib
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState, useEffect } from "react";
+
+export default function CalendarComps() {
+  const [calendarVisible, setCalendarVisible] = useState(false);
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
+  const toggleCalendar = () => {
+    setCalendarVisible(!calendarVisible);
+  };
+
+  useEffect(() => {
+    setCurrentDate(new Date());
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Popover open={calendarVisible} onOpenChange={setCalendarVisible}>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn("w-[240px] justify-start text-left font-normal")}
+              onClick={toggleCalendar}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {currentDate ? (
+                format(currentDate, "PPP")
+              ) : (
+                <span>Pick a date</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={undefined}
+              onSelect={() => {}}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+}
