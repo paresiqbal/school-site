783658b0376049608dfa5d09304122ab8f75aa1a
paresiqbal <thecrowdel@gmail.com>
2024-10-11 19:58:07 +0700

    Refactor loading component: Improve loading animation and layout

--- app/(admin)/dashboard/news/loading.tsx
+++ app/(admin)/dashboard/news/loading.tsx
@@ -7,33 +7,28 @@ export default function loading() {
         <div className="h-4 w-24 animate-pulse rounded-md bg-muted-foreground/40"></div>
       </div>
 
-      {[...Array(3)].map((_, i) => (
-        <div
-          key={i}
-          className="mb-4 flex animate-pulse flex-col p-4 md:flex-row md:items-center"
-        >
-          <div className="mb-4 w-full md:mb-0 md:mr-4 md:w-1/4">
-            <div className="h-64 w-full rounded-lg bg-zinc-500"></div>
+      <div className="mb-4 flex animate-pulse flex-col p-4 md:flex-row md:items-center">
+        <div className="mb-4 w-full md:mb-0 md:mr-4 md:w-1/4">
+          <div className="h-64 w-full rounded-lg bg-zinc-500"></div>
+        </div>
+
+        <div className="w-full md:w-3/4">
+          <div className="mb-2">
+            <div className="mb-2 h-6 w-48 rounded-md bg-zinc-500"></div>
+
+            <div className="h-4 w-32 rounded-md bg-zinc-500"></div>
           </div>
 
-          <div className="w-full md:w-3/4">
-            <div className="mb-2">
-              <div className="mb-2 h-6 w-48 rounded-md bg-zinc-500"></div>
+          <div className="mb-4">
+            <div className="h-20 w-full rounded-md bg-zinc-500"></div>
+          </div>
 
-              <div className="h-4 w-32 rounded-md bg-zinc-500"></div>
-            </div>
-
-            <div className="mb-4">
-              <div className="h-20 w-full rounded-md bg-zinc-500"></div>
-            </div>
-
-            <div className="flex gap-4">
-              <div className="h-10 w-24 rounded-md bg-zinc-500"></div>
-              <div className="h-10 w-24 rounded-md bg-zinc-500"></div>
-            </div>
+          <div className="flex gap-4">
+            <div className="h-10 w-24 rounded-md bg-zinc-500"></div>
+            <div className="h-10 w-24 rounded-md bg-zinc-500"></div>
           </div>
         </div>
-      ))}
+      </div>
 
       <div className="mt-4 h-10 w-36 animate-pulse rounded-md bg-zinc-500"></div>
     </div>
