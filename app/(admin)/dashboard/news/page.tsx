c423f58c2abd93d679cd8bf61b612d6366c2f07a
paresiqbal <thecrowdel@gmail.com>
2024-10-11 19:57:04 +0700

    Refactor loading component: Improve loading animation and layout

--- app/(admin)/dashboard/news/page.tsx
+++ app/(admin)/dashboard/news/page.tsx
@@ -146,7 +146,7 @@ export default function ListNews() {
           {item.image && (
             <div className="mb-4 w-full md:mb-0 md:mr-4 md:w-1/4">
               <Image
-                src={`http://localhost:8000/storage/${item.image}`}
+                src={`http://localhost:8000/${item.image}`}
                 alt={item.title}
                 width={300}
                 height={250}
