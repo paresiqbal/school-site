c1729f79815aa4b8c1fb54a8829f20ae2af6c6f9
paresiqbal <thecrowdel@gmail.com>
2024-10-17 15:07:26 +0700

    Refactor: Update facility names, icons, and descriptions
    - Update facility names to Indonesian language
    - Update facility icons to reflect their purpose more accurately
    - Update facility descriptions to provide clearer information

--- app/(landing)/facility/page.tsx
+++ app/(landing)/facility/page.tsx
@@ -15,104 +15,111 @@ import {
   School,
   Book,
   FlaskConical,
-  Music,
+  Camera,
   Dumbbell,
   Utensils,
   Monitor,
   Users,
   Cross,
+  ArrowRight,
 } from "lucide-react";
 
 export default function Facility() {
   const facilities = [
     {
       id: 1,
-      name: "Classrooms",
+      name: "Kelas",
       icon: School,
       image: "/assets/facility/class.svg",
       description:
-        "attend their daily lessons and engage in learning activities",
-      slug: "classrooms",
+        "Siswa dapat mengikuti pelajaran harian dan terlibat dalam kegiatan pembelajaran di kelas.",
+      slug: "kelas",
     },
     {
       id: 2,
-      name: "Library",
+      name: "Perpustakaan",
       icon: Book,
       image: "/assets/facility/library.svg",
-      description: "read books, study, and conduct research",
-      slug: "library",
+      description:
+        "Membaca buku, belajar, dan melakukan penelitian di perpustakaan.",
+      slug: "perpustakaan",
     },
     {
       id: 3,
-      name: "Science Lab",
+      name: "Laboratorium Sains",
       icon: FlaskConical,
       image: "/assets/facility/lab.svg",
-      description: "perform experiments and learn about scientific concepts",
-      slug: "science-lab",
+      description: "Melakukan eksperimen dan mempelajari konsep ilmiah.",
+      slug: "laboratorium-sains",
     },
     {
       id: 4,
-      name: "Music Room",
-      icon: Music,
+      name: "Studio",
+      icon: Camera,
       image: "/assets/facility/studio.svg",
-      description: "practice instruments and participate in music classes",
-      slug: "music-room",
+      description: "Belajar fotografi, dan videografi.",
+      slug: "studio",
     },
     {
       id: 5,
-      name: "Gymnasium",
+      name: "Lapangan",
       icon: Dumbbell,
       image: "/assets/facility/gym.svg",
-      description: "engage in physical education and sports activities",
-      slug: "gymnasium",
+      description: "Kegiatan pendidikan jasmani dan olahraga",
+      slug: "lapangan",
     },
     {
       id: 6,
-      name: "Cafeteria",
+      name: "Kantin",
       icon: Utensils,
       image: "/assets/facility/cafe.svg",
-      description: "eat meals and socialize during lunch breaks",
-      slug: "cafeteria",
+      description: "Makan dan bersosialisasi selama jam istirahat makan siang",
+      slug: "kantin",
     },
     {
       id: 7,
-      name: "Computer Lab",
+      name: "Lab Komputer",
       icon: Monitor,
       image: "/assets/facility/computer.svg",
-      description: "learn computer skills and work on digital projects",
-      slug: "computer-lab",
+      description:
+        "Mempelajari keterampilan komputer dan mengerjakan proyek digital",
+      slug: "lab-komputer",
     },
     {
       id: 8,
-      name: "Auditorium",
+      name: "Lab Bahasa",
       icon: Users,
       image: "/assets/facility/audit.svg",
-      description: "attend assemblies, performances, and large gatherings",
-      slug: "auditorium",
+      description: "Mempelajari bahasa asing dan berlatih berbicara",
+      slug: "lab-bahasa",
     },
     {
       id: 9,
-      name: "Nurse's Office",
+      name: "Usaha Kesehatan Sekolah",
       icon: Cross,
       image: "/assets/facility/hospital.svg",
-      description: "receive medical attention and health services",
-      slug: "nurses-office",
+      description: "Menerima perhatian medis dan layanan kesehatan",
+      slug: "uks",
     },
   ];
 
   return (
-    <div className="container mx-auto mt-4 flex max-w-[1200px] flex-col pt-6 md:pt-12">
-      <h1 className="mb-8 text-center text-3xl font-bold">Fasilitas Sekolah</h1>
-      <h2 className="pb-8 text-xl">
-        Temukan berbagai ruang dan sumber daya yang dirancang untuk meningkatkan
-        pembelajaran dan kesejahteraan siswa.
-      </h2>
+    <div className="container mx-auto mb-8 mt-4 flex max-w-[1200px] flex-col pt-6 md:pt-12">
+      <div className="space-y-2 pb-8 text-center">
+        <h1 className="mb-8 text-center text-3xl font-bold">
+          Fasilitas Sekolah
+        </h1>
+        <p className="text-md mb-12 text-muted-foreground md:text-lg">
+          Temukan berbagai ruang dan sumber daya yang dirancang untuk
+          meningkatkan pembelajaran dan kesejahteraan siswa.
+        </p>
+      </div>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {facilities.map((facility, index) => (
           <Card
             key={index}
-            className="transition-shadow duration-300 hover:scale-105 hover:shadow-lg"
+            className="bg-yellow-50 transition-shadow duration-300 hover:scale-105 hover:shadow-lg"
           >
             <CardHeader className="flex flex-row items-center space-x-4 pb-2">
               <facility.icon
@@ -123,7 +130,7 @@ export default function Facility() {
             </CardHeader>
             <CardContent>
               <p className="text-sm text-muted-foreground">
-                This is where students can {facility.description}.
+                {facility.description}.
               </p>
             </CardContent>
             <CardFooter className="flex flex-col pt-2">
@@ -136,9 +143,10 @@ export default function Facility() {
               />
               <Link
                 href={`/facility/${facility.slug}`}
-                className="hover:underline"
+                className="flex items-center justify-center decoration-emerald-500 underline-offset-2 hover:underline"
               >
-                Learn More
+                Pelajari Lebih Lanjut
+                <ArrowRight className="ml-1 h-4 w-4" />
               </Link>
             </CardFooter>
           </Card>
