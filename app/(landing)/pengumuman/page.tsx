d62f2514c331ce581ca66b1eb564f77c9b143982
paresiqbal <thecrowdel@gmail.com>
2024-10-26 14:35:48 +0700

    Add Pengumuman page with Announcement component

---
+++ app/(landing)/pengumuman/page.tsx
@@ -0,0 +1,10 @@
+import AnnouncementComps from "@/components/Announcement";
+
+export default function Pengumuman() {
+  return (
+    <div>
+      <h1>Pengumuman</h1>
+      <AnnouncementComps />
+    </div>
+  );
+}
