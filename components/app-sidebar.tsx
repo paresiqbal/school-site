2ebdc398ed2b630eb04cbb43746c0ec33e966799
paresiqbal <thecrowdel@gmail.com>
2024-10-28 20:06:52 +0700

    Refactor AppSidebar component to update team logos and improve responsiveness

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import * as React from "react";
 import { BookOpen, Bot, Hexagon, SquareTerminal } from "lucide-react";
 
 import { NavMain } from "@/components/nav-main";
@@ -10,13 +9,10 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  SidebarMenu,
   SidebarMenuButton,
-  SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
 
-// This is sample data.
 const data = {
   user: {
     name: "shadcn",
@@ -32,11 +28,11 @@ const data = {
       items: [
         {
           title: "List Berita",
-          url: "/news",
+          url: "/dashboard/news",
         },
         {
           title: "Buat Berita",
-          url: "/news/createNews",
+          url: "/dashboard/news/createNews",
         },
       ],
     },
@@ -89,23 +85,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props} variant="sidebar">
       <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <div>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-              >
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Hexagon size={20} />
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <p>SMKN 1 RL</p>
-                </div>
-              </SidebarMenuButton>
+        <div>
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
+              <Hexagon size={20} />
             </div>
-          </SidebarMenuItem>
-        </SidebarMenu>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <p>SMKN 1 RL</p>
+            </div>
+          </SidebarMenuButton>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
