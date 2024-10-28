36566bbdeb0625d565a31b0210160d6e65ac6c3f
paresiqbal <thecrowdel@gmail.com>
2024-10-28 20:54:06 +0700

    Refactor Dashboard page component to remove unused components and tabs

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -3,15 +3,10 @@
 import { useSession } from "next-auth/react";
 
 // components
-import NewsComps from "@/components/News";
 import CalendarComps from "@/components/Calendar";
-import AnnouncementComps from "@/components/Announcement";
-import AgendaComps from "@/components/Agenda";
 import Topbar from "@/components/Topbar";
 
 // ui lib
-import { Card } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -48,35 +43,6 @@ export default function Dashboard() {
             </h1>
           </>
         )}
-      </div>
-      <div className="pt-10">
-        <Tabs defaultValue="account" className="w-full sm:w-10/12">
-          <TabsList className="mb-10 grid w-full grid-cols-2 sm:grid-cols-4 lg:mb-2">
-            <TabsTrigger value="News">News</TabsTrigger>
-            <TabsTrigger value="Announcement">Announcement</TabsTrigger>
-            <TabsTrigger value="Agenda">Agenda</TabsTrigger>
-            <TabsTrigger value="Gallery">Gallery</TabsTrigger>
-          </TabsList>
-
-          <TabsContent value="News">
-            <Card className="p-2">
-              <NewsComps />
-            </Card>
-          </TabsContent>
-          <TabsContent value="Announcement">
-            <Card>
-              <AnnouncementComps />
-            </Card>
-          </TabsContent>
-          <TabsContent value="Agenda">
-            <Card>
-              <AgendaComps />
-            </Card>
-          </TabsContent>
-          <TabsContent value="Gallery">
-            <p>components here</p>
-          </TabsContent>
-        </Tabs>
       </div>
     </div>
   );
