e6ce8547ba4c5177515180c4bae1d28a2afbb6cb
paresiqbal <thecrowdel@gmail.com>
2024-10-28 18:57:51 +0700

    Update Sidebar component to remove SidebarTrigger and add Topbar component

---
+++ components/Topbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SidebarTrigger } from "./ui/sidebar";
+
+export default function Topbar() {
+  return (
+    <div>
+      <SidebarTrigger className="-ml-1" />
+    </div>
+  );
+}
