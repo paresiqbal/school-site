c7c26abd00f79c94c604b6f1f3cbd5cf3623c5e3
paresiqbal <thecrowdel@gmail.com>
2024-10-30 15:18:24 +0700

    Update page components with localized titles and improved layout

--- app/(landing)/guide/academic/page.tsx
+++ app/(landing)/guide/academic/page.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-
 export default function Academic() {
-  return <div>Academic</div>;
+  return (
+    <div className="container mx-auto mb-8 flex max-w-[1200px] flex-col px-4 pt-6 md:px-0 md:pt-12">
+      <div className="pb-2">
+        <h1 className="mb-2 text-3xl font-bold underline decoration-rose-500 underline-offset-8 md:mb-6 md:text-5xl">
+          Akademik
+        </h1>
+      </div>
+    </div>
+  );
 }
