c8b7e0898131620240886f5db8f67efb51b1f02d
paresiqbal <thecrowdel@gmail.com>
2024-10-30 14:31:11 +0700

    Update contact page

--- app/(landing)/contact/page.tsx
+++ app/(landing)/contact/page.tsx
@@ -46,11 +46,19 @@ export default function contact() {
         </div>
       </div>
 
-      {/* Map Placeholder */}
+      {/* Google Maps Embed */}
       <div className="mt-6">
         <h2 className="mb-2 text-xl font-semibold">Peta Lokasi</h2>
-        <div className="flex h-64 w-full items-center justify-center rounded-md bg-gray-300">
-          <p className="text-gray-700">Map Placeholder</p>
+        <div className="flex h-64 w-full items-center justify-center overflow-hidden rounded-md">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.4784694796654!2d102.52561257501705!3d-3.4759321419400346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e31398ac84d0839%3A0x7d802d8f5e45b940!2sSMK%20NEGERI%201%20REJANG%20LEBONG!5e0!3m2!1sen!2sid!4v1730273311528!5m2!1sen!2sid"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </div>
