c3b332b1e03a0ecf8604bbb622bddf975e5824be
paresiqbal <thecrowdel@gmail.com>
2024-10-31 19:28:13 +0700

    Update calendar academic page title and add high-quality image for improved presentation

--- app/(landing)/guide/calendar-academic/page.tsx
+++ app/(landing)/guide/calendar-academic/page.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
+
 export default function CalendarAcademic() {
   return (
     <div className="container mx-auto mb-8 flex max-w-[1200px] flex-col px-4 pt-6 md:px-0 md:pt-12">
       <div className="pb-2">
         <h1 className="mb-2 text-3xl font-bold underline decoration-rose-500 underline-offset-8 md:mb-6 md:text-5xl">
-          Akademik
+          Kalender Akademik
         </h1>
       </div>
+
+      <Image
+        src="/assets/kalender.jpg"
+        width={1800}
+        height={1500}
+        quality={100}
+        priority
+        alt="Kalender Akademik"
+      />
     </div>
   );
 }
