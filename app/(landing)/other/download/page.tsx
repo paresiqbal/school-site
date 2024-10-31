857661ccdcdb4003ffcf1a02cbfb6a866cbf7853
paresiqbal <thecrowdel@gmail.com>
2024-10-31 14:36:14 +0700

    Add download section with links to educational materials

--- app/(landing)/other/download/page.tsx
+++ app/(landing)/other/download/page.tsx
@@ -1,12 +1,30 @@
-import React from "react";
-
 export default function Download() {
+  const downloads = [
+    { title: "Document 1", link: "/path/to/document1.pdf" },
+    { title: "Document 2", link: "/path/to/document2.pdf" },
+    { title: "Document 3", link: "/path/to/document3.pdf" },
+  ];
+
   return (
-    <div className="container mx-auto mb-8 flex max-w-[1200px] flex-col px-4 pt-6 md:px-0 md:pt-12">
+    <div className="container mx-auto mb-8 max-w-[1200px] px-4 pt-6 md:px-0 md:pt-12">
       <div className="pb-2">
         <h1 className="mb-2 text-3xl font-bold underline decoration-rose-500 underline-offset-8 md:mb-6 md:text-5xl">
           Download & Links
         </h1>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <p>Materi Pembelajaran</p>
+        {downloads.map((download, index) => (
+          <a
+            key={index}
+            href={download.link}
+            download
+            className="text-blue-500 hover:underline"
+          >
+            {download.title}
+          </a>
+        ))}
       </div>
     </div>
   );
