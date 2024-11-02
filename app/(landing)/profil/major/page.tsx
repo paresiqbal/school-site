29f62b06a373eccef7a350ae55416196b9342352
paresiqbal <thecrowdel@gmail.com>
2024-11-02 14:51:04 +0700

    Add Major component and update image domains in Next.js config

---
+++ app/(landing)/profil/major/page.tsx
@@ -0,0 +1,3 @@
+export default function Major() {
+  return <div>Major</div>;
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["laravel.smkn1rl.sch.id"],
+    domains: ["127.0.0.1", "localhost"],
   },
 };
 
