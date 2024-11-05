ed11f66a46958e7a9bb187d8fe51af0ca83b85ce
paresiqbal <thecrowdel@gmail.com>
2024-11-05 22:42:08 +0700

    Refactor Download component for improved layout, styling, and accessibility

--- app/(landing)/guide/download/page.tsx
+++ app/(landing)/guide/download/page.tsx
@@ -24,25 +24,28 @@ export default function Download() {
   ];
 
   return (
-    <div className="container mx-auto mb-8 max-w-[1200px] px-4 pt-6 md:px-0 md:pt-12">
-      <div className="pb-2">
-        <h1 className="mb-2 text-3xl font-bold underline decoration-blue-500 underline-offset-8 md:mb-6 md:text-5xl">
+    <div className="mx-auto mb-8 max-w-[1200px] px-4 pt-6 md:px-0 md:pt-12">
+      <div className="pb-4 text-center">
+        <h1 className="mb-4 text-3xl font-bold underline decoration-blue-500 underline-offset-8 md:text-5xl">
           Download & Links
         </h1>
       </div>
 
       {downloadGroups.map((group, index) => (
-        <div key={index} className="mb-4">
-          <h2 className="text-2xl font-semibold text-gray-800">
+        <div
+          key={index}
+          className="mb-6 border-4 border-black p-4 shadow-[8px_8px_0px_rgba(0,0,0,1)] dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)]"
+        >
+          <h2 className="text-2xl font-bold text-gray-800 dark:text-white">
             {group.category}
           </h2>
-          <div className="mt-2 flex flex-col gap-1">
+          <div className="mt-4 space-y-2">
             {group.items.map((item, itemIndex) => (
               <a
                 key={itemIndex}
                 href={item.link}
                 download
-                className="hover:underline"
+                className="block border-2 border-black bg-gray-100 px-4 py-2 text-lg font-semibold hover:bg-gray-200 dark:border-white dark:bg-gray-800 dark:hover:bg-gray-700"
               >
                 {item.title}
               </a>
