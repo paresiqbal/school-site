bc0a9660c48aa140b93bab26ed3e5b6742c7e9b6
paresiqbal <thecrowdel@gmail.com>
2024-11-05 22:23:05 +0700

    Refactor Tbsm component for improved layout, styling, and carousel integration

--- app/(landing)/major/tbsm/page.tsx
+++ app/(landing)/major/tbsm/page.tsx
@@ -2,19 +2,10 @@
 
 import * as React from "react";
 import Link from "next/link";
-
-// ex lib
 import Autoplay from "embla-carousel-autoplay";
 
 // ui lib
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -31,15 +22,16 @@ export default function Tbsm() {
   );
 
   return (
-    <div className="container mx-auto mb-8 flex max-w-[1200px] flex-col px-4 pt-6 md:px-6 md:pt-12 lg:px-0">
-      <div>
-        <h1 className="mb-2 text-2xl font-bold md:mb-6 md:text-4xl lg:text-5xl">
+    <div className="mx-auto mt-4 flex max-w-[1200px] flex-col pt-6 md:pt-12">
+      <div className="space-y-2 pb-8">
+        <h1 className="mb-2 text-4xl font-bold dark:text-gray-300 md:mb-6 lg:text-5xl">
           Teknik Bisnis Sepeda Motor
         </h1>
-        <p className="mb-6 text-base text-muted-foreground md:text-lg lg:text-xl">
-          Selami dunia teknik sepeda motor dan bisnisnya
+        <p className="text-md mb-12 text-muted-foreground md:text-lg lg:text-xl">
+          Selami dunia teknik sepeda motor dan bisnisnya.
         </p>
       </div>
+
       <div className="grid gap-6 lg:grid-cols-3">
         <div className="lg:col-span-2">
           <Carousel
@@ -52,21 +44,20 @@ export default function Tbsm() {
               {Array.from({ length: 5 }).map((_, index) => (
                 <CarouselItem key={index}>
                   <div className="p-1">
-                    <Card className="aspect-[4/3] w-full">
-                      {/* Set a 4:3 aspect ratio */}
-                      <CardContent className="flex h-full items-center justify-center p-4 md:p-6">
+                    <div className="aspect-[4/3] w-full border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] dark:border-white">
+                      <div className="flex h-full items-center justify-center p-4 md:p-6">
                         <span className="text-3xl font-semibold md:text-4xl lg:text-5xl">
                           {index + 1}
                         </span>
-                      </CardContent>
-                    </Card>
+                      </div>
+                    </div>
                   </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
           </Carousel>
 
-          <div className="prose mt-4 max-w-none text-sm md:text-base lg:text-lg">
+          <div className="my-8 max-w-none text-sm md:text-base lg:text-lg">
             <p>
               Teknik Bisnis Sepeda Motor adalah program keahlian yang
               mempelajari teknik sepeda motor dan bisnisnya. Program ini
@@ -75,48 +66,45 @@ export default function Tbsm() {
             </p>
           </div>
         </div>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-lg md:text-xl lg:text-2xl">
-              Program Sorotan
-            </CardTitle>
-            <CardDescription className="text-sm md:text-base">
-              Program Teknik Bisnis Sepeda Motor menawarkan:
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-3 md:gap-4">
-            <div className="flex items-center gap-3 md:gap-4">
-              <BookOpen className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
+
+        <div className="rounded-md border-4 border-black bg-emerald-300/80 p-4 text-gray-800 shadow-[8px_8px_0px_rgba(0,0,0,1)] hover:bg-emerald-400 dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)]">
+          <h3 className="mb-2 text-2xl font-bold">Program Sorotan</h3>
+          <p className="mb-4 text-base font-medium">
+            Program Teknik Bisnis Sepeda Motor menawarkan:
+          </p>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3">
+              <BookOpen className="h-6 w-6" />
+              <span className="text-base font-semibold">
                 Kurikulum Komprehensif
-              </div>
+              </span>
             </div>
-            <div className="flex items-center gap-3 md:gap-4">
-              <Users className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
+            <div className="flex items-center gap-3">
+              <Users className="h-6 w-6" />
+              <span className="text-base font-semibold">
                 Proyek Kolaboratif
-              </div>
+              </span>
             </div>
-            <div className="flex items-center gap-3 md:gap-4">
-              <BriefcaseIcon className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
-                Magang Industri
-              </div>
+            <div className="flex items-center gap-3">
+              <BriefcaseIcon className="h-6 w-6" />
+              <span className="text-base font-semibold">Magang Industri</span>
             </div>
-            <div className="flex items-center gap-3 md:gap-4">
-              <Hammer className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
-                Peluang Kerja
-              </div>
+            <div className="flex items-center gap-3">
+              <Hammer className="h-6 w-6" />
+              <span className="text-base font-semibold">Peluang Kerja</span>
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       </div>
 
       <Tabs defaultValue="courses" className="mt-8 md:mt-12">
-        <TabsList>
-          <TabsTrigger value="courses">Materi Utama</TabsTrigger>
-          <TabsTrigger value="careers">Prospek Karir</TabsTrigger>
+        <TabsList className="border-4 border-black">
+          <TabsTrigger value="courses" className="font-bold">
+            Materi Utama
+          </TabsTrigger>
+          <TabsTrigger value="careers" className="font-bold">
+            Prospek Karir
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="courses" className="mt-4 md:mt-6">
           <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
@@ -131,11 +119,12 @@ export default function Tbsm() {
               "Praktik Kerja Lapangan (PKL)",
               "Over Houl Engine",
             ].map((course) => (
-              <Card key={course}>
-                <CardHeader>
-                  <CardTitle className="text-sm md:text-lg">{course}</CardTitle>
-                </CardHeader>
-              </Card>
+              <div
+                key={course}
+                className="transform border-4 border-black bg-emerald-300/80 p-4 text-sm font-semibold text-gray-800 shadow-[8px_8px_0px_rgba(0,0,0,1)] transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)] dark:hover:shadow-[4px_4px_0px_rgba(255,255,255,1)] md:text-lg"
+              >
+                {course}
+              </div>
             ))}
           </div>
         </TabsContent>
@@ -149,21 +138,26 @@ export default function Tbsm() {
               "Sales & Marketing",
               "Teknisi Field Service",
             ].map((career) => (
-              <Card key={career}>
-                <CardHeader>
-                  <CardTitle className="text-sm md:text-lg">{career}</CardTitle>
-                </CardHeader>
-              </Card>
+              <div
+                key={career}
+                className="transform border-4 border-black bg-emerald-300/80 p-4 text-sm font-semibold text-gray-800 shadow-[8px_8px_0px_rgba(0,0,0,1)] transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)] dark:hover:shadow-[4px_4px_0px_rgba(255,255,255,1)] md:text-lg"
+              >
+                {career}
+              </div>
             ))}
           </div>
         </TabsContent>
       </Tabs>
 
-      <div className="mt-8 text-center md:mt-12">
+      <div className="mb-8 mt-8 text-center md:mt-12">
         <h2 className="mb-4 text-lg font-bold md:text-xl lg:text-2xl">
           Siap untuk memulai karir Anda di dunia sepeda motor?
         </h2>
-        <Button asChild size="lg">
+        <Button
+          asChild
+          size="lg"
+          className="hover:shadow-button border-4 border-black dark:border-white"
+        >
           <Link href="/guide/registration">Daftar Sekarang</Link>
         </Button>
       </div>
