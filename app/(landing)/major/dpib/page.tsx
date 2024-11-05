377739d6e656324c1b2ce6af8e0346136b2354ac
paresiqbal <thecrowdel@gmail.com>
2024-11-05 22:15:56 +0700

    Refactor DesainPemodelan component to integrate image carousel, enhance layout, and improve styling

--- app/(landing)/major/dpib/page.tsx
+++ app/(landing)/major/dpib/page.tsx
@@ -2,19 +2,13 @@
 
 import * as React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 // ex lib
 import Autoplay from "embla-carousel-autoplay";
 
 // ui lib
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -30,17 +24,26 @@ export default function DesainPemodelan() {
     Autoplay({ delay: 2000, stopOnInteraction: true }),
   );
 
+  const images = [
+    "/assets/major/desain/desain1.jpg",
+    "/assets/major/desain/desain2.jpg",
+    "/assets/major/desain/desain3.jpg",
+    "/assets/major/desain/desain4.jpg",
+    "/assets/major/desain/desain5.jpg",
+  ];
+
   return (
-    <div className="container mx-auto mb-8 flex max-w-[1200px] flex-col px-4 pt-6 md:px-6 md:pt-12 lg:px-0">
-      <div>
-        <h1 className="mb-2 text-2xl font-bold md:mb-6 md:text-4xl lg:text-5xl">
+    <div className="mx-auto mt-4 flex max-w-[1200px] flex-col pt-6 md:pt-12">
+      <div className="space-y-2 pb-8">
+        <h1 className="mb-2 text-balance text-4xl font-bold dark:text-gray-300 md:mb-6 md:text-5xl">
           Desain Pemodelan
         </h1>
-        <p className="mb-6 text-base text-muted-foreground md:text-lg lg:text-xl">
+        <p className="text-md mb-12 text-muted-foreground md:text-lg">
           Pelajari teknik desain pemodelan dan siapkan diri Anda untuk menjadi
           arsitek.
         </p>
       </div>
+
       <div className="grid gap-6 lg:grid-cols-3">
         <div className="lg:col-span-2">
           <Carousel
@@ -50,24 +53,25 @@ export default function DesainPemodelan() {
             onMouseLeave={plugin.current.reset}
           >
             <CarouselContent>
-              {Array.from({ length: 5 }).map((_, index) => (
+              {images.map((src, index) => (
                 <CarouselItem key={index}>
                   <div className="p-1">
-                    <Card className="aspect-[4/3] w-full">
-                      {/* Set a 4:3 aspect ratio */}
-                      <CardContent className="flex h-full items-center justify-center p-4 md:p-6">
-                        <span className="text-3xl font-semibold md:text-4xl lg:text-5xl">
-                          {index + 1}
-                        </span>
-                      </CardContent>
-                    </Card>
+                    <div className="aspect-[4/3] w-full border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] dark:border-white">
+                      <Image
+                        src={src}
+                        alt={`Desain Image ${index + 1}`}
+                        width={800}
+                        height={600}
+                        className="h-full w-full rounded-md object-cover"
+                      />
+                    </div>
                   </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
           </Carousel>
 
-          <div className="prose mt-4 max-w-none text-sm md:text-base lg:text-lg">
+          <div className="my-8 max-w-none text-sm md:text-base lg:text-lg">
             <p>
               Teknik desain pemodelan adalah salah satu program studi yang
               menawarkan kurikulum komprehensif, proyek kolaboratif, magang
@@ -79,48 +83,45 @@ export default function DesainPemodelan() {
             </p>
           </div>
         </div>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-lg md:text-xl lg:text-2xl">
-              Program Sorotan
-            </CardTitle>
-            <CardDescription className="text-sm md:text-base">
-              Program Teknik Desain Pemodelan menawarkan:
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-3 md:gap-4">
-            <div className="flex items-center gap-3 md:gap-4">
-              <BookOpen className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
+
+        <div className="rounded-md border-4 border-black bg-emerald-300/80 p-4 text-gray-800 shadow-[8px_8px_0px_rgba(0,0,0,1)] hover:bg-emerald-400 dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)]">
+          <h3 className="mb-2 text-2xl font-bold">Program Sorotan</h3>
+          <p className="mb-4 text-base font-medium">
+            Program Teknik Desain Pemodelan menawarkan:
+          </p>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3">
+              <BookOpen className="h-6 w-6" />
+              <span className="text-base font-semibold">
                 Kurikulum Komprehensif
-              </div>
+              </span>
             </div>
-            <div className="flex items-center gap-3 md:gap-4">
-              <Users className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
+            <div className="flex items-center gap-3">
+              <Users className="h-6 w-6" />
+              <span className="text-base font-semibold">
                 Proyek Kolaboratif
-              </div>
+              </span>
             </div>
-            <div className="flex items-center gap-3 md:gap-4">
-              <BriefcaseIcon className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
-                Magang Industri
-              </div>
+            <div className="flex items-center gap-3">
+              <BriefcaseIcon className="h-6 w-6" />
+              <span className="text-base font-semibold">Magang Industri</span>
             </div>
-            <div className="flex items-center gap-3 md:gap-4">
-              <Hammer className="h-5 w-5 md:h-6 md:w-6" />
-              <div className="text-sm font-medium md:text-base">
-                Peluang Kerja
-              </div>
+            <div className="flex items-center gap-3">
+              <Hammer className="h-6 w-6" />
+              <span className="text-base font-semibold">Peluang Kerja</span>
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       </div>
 
       <Tabs defaultValue="courses" className="mt-8 md:mt-12">
-        <TabsList>
-          <TabsTrigger value="courses">Materi Utama</TabsTrigger>
-          <TabsTrigger value="careers">Prospek Karir</TabsTrigger>
+        <TabsList className="border-4 border-black">
+          <TabsTrigger value="courses" className="font-bold">
+            Materi Utama
+          </TabsTrigger>
+          <TabsTrigger value="careers" className="font-bold">
+            Prospek Karir
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="courses" className="mt-4 md:mt-6">
           <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
@@ -133,16 +134,17 @@ export default function DesainPemodelan() {
               "Desain Produk",
               "Teknologi Material",
               "Proses Manufaktur dan Produksi",
-              "Pemahaman Struktur dan Konstruksi:",
+              "Pemahaman Struktur dan Konstruksi",
               "Analisis dan Simulasi",
               "Teknik Presentasi Desain",
               "Praktik Kerja Lapangan (PKL)",
             ].map((course) => (
-              <Card key={course}>
-                <CardHeader>
-                  <CardTitle className="text-sm md:text-lg">{course}</CardTitle>
-                </CardHeader>
-              </Card>
+              <div
+                key={course}
+                className="transform border-4 border-black bg-emerald-300/80 p-4 text-sm font-semibold text-gray-800 shadow-[8px_8px_0px_rgba(0,0,0,1)] transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)] dark:hover:shadow-[4px_4px_0px_rgba(255,255,255,1)] md:text-lg"
+              >
+                {course}
+              </div>
             ))}
           </div>
         </TabsContent>
@@ -162,21 +164,26 @@ export default function DesainPemodelan() {
               "Desainer Proyek Infrastruktur",
               "Desainer Visualisasi untuk Real Estate",
             ].map((career) => (
-              <Card key={career}>
-                <CardHeader>
-                  <CardTitle className="text-sm md:text-lg">{career}</CardTitle>
-                </CardHeader>
-              </Card>
+              <div
+                key={career}
+                className="transform border-4 border-black bg-emerald-300/80 p-4 text-sm font-semibold text-gray-800 shadow-[8px_8px_0px_rgba(0,0,0,1)] transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)] dark:hover:shadow-[4px_4px_0px_rgba(255,255,255,1)] md:text-lg"
+              >
+                {career}
+              </div>
             ))}
           </div>
         </TabsContent>
       </Tabs>
 
-      <div className="mt-8 text-center md:mt-12">
+      <div className="mb-8 mt-8 text-center md:mt-12">
         <h2 className="mb-4 text-lg font-bold md:text-xl lg:text-2xl">
           Siap untuk memulai karir Anda di bidang desain pemodelan?
         </h2>
-        <Button asChild size="lg">
+        <Button
+          asChild
+          size="lg"
+          className="hover:shadow-button border-4 border-black dark:border-white"
+        >
           <Link href="/guide/registration">Daftar Sekarang</Link>
         </Button>
       </div>
