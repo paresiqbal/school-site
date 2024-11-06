441a5240afcab931a83321750ed26551cd0f01cf
paresiqbal <thecrowdel@gmail.com>
2024-11-06 21:52:15 +0700

    Refactor Download component for improved styling and hover effects

--- app/(landing)/guide/download/page.tsx
+++ app/(landing)/guide/download/page.tsx
@@ -24,9 +24,9 @@ export default function Download() {
   ];
 
   return (
-    <div className="mx-auto mb-8 max-w-[1200px] px-4 pt-6 md:px-0 md:pt-12">
+    <div className="mx-auto mb-8 max-w-[1200px] pt-6 md:pt-12">
       <div className="pb-4 text-center">
-        <h1 className="mb-4 text-3xl font-bold underline decoration-blue-500 underline-offset-8 md:text-5xl">
+        <h1 className="mb-4 text-3xl font-bold md:text-5xl">
           Download & Links
         </h1>
       </div>
@@ -34,7 +34,7 @@ export default function Download() {
       {downloadGroups.map((group, index) => (
         <div
           key={index}
-          className="mb-6 border-4 border-black p-4 shadow-[8px_8px_0px_rgba(0,0,0,1)] dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)]"
+          className="mb-6 border-4 border-black p-4 hover:shadow-card dark:border-white"
         >
           <h2 className="text-2xl font-bold text-gray-800 dark:text-white">
             {group.category}
@@ -45,7 +45,7 @@ export default function Download() {
                 key={itemIndex}
                 href={item.link}
                 download
-                className="block border-2 border-black bg-gray-100 px-4 py-2 text-lg font-semibold hover:bg-gray-200 dark:border-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                className="block border-2 border-black bg-indigo-500/80 px-4 py-2 font-semibold active:bg-indigo-500 dark:border-white"
               >
                 {item.title}
               </a>
