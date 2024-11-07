ba95baef14cc42ba2784703a737cf1a7115e416b
paresiqbal <thecrowdel@gmail.com>
2024-11-07 15:10:55 +0700

    Refactor Tkj component for improved styling and consistency

--- app/(landing)/major/tkj/page.tsx
+++ app/(landing)/major/tkj/page.tsx
@@ -35,7 +35,7 @@ export default function Tkj() {
   return (
     <div className="mx-auto mt-4 flex max-w-[1200px] flex-col pt-6 md:pt-12">
       <div className="space-y-2 pb-8">
-        <h1 className="mb-2 text-balance text-4xl font-bold dark:text-gray-300 md:mb-6 md:text-5xl">
+        <h1 className="mb-2 text-balance text-4xl font-bold md:mb-6 md:text-5xl">
           Teknik Komputer & Jaringan
         </h1>
         <p className="text-md mb-12 text-muted-foreground md:text-lg">
@@ -70,7 +70,6 @@ export default function Tkj() {
 
           <div className="my-8 max-w-none text-sm md:text-base lg:text-lg">
             <p className="text-lg">
-              Program{" "}
               <span className="text-primary">
                 Teknik Komputer & Jaringan (TKJ)
               </span>{" "}
@@ -82,7 +81,7 @@ export default function Tkj() {
           </div>
         </div>
 
-        <div className="rounded-md border-2 border-foreground bg-emerald-300/90 p-4 transition hover:bg-emerald-400 hover:shadow-card">
+        <div className="rounded-md border-2 border-foreground bg-emerald-300/90 p-4 transition hover:bg-emerald-400 hover:shadow-card dark:text-background">
           <h3 className="mb-2 text-2xl font-bold">Program Sorotan</h3>
           <p className="mb-4 text-base font-medium">
             Program Teknik Komputer & Jaringan menawarkan:
@@ -113,7 +112,7 @@ export default function Tkj() {
       </div>
 
       <Tabs defaultValue="courses" className="mt-8 md:mt-12">
-        <TabsList className="border-4 border-black">
+        <TabsList className="border-4 border-foreground">
           <TabsTrigger value="courses" className="font-bold">
             Materi Utama
           </TabsTrigger>
@@ -136,7 +135,7 @@ export default function Tkj() {
             ].map((course) => (
               <div
                 key={course}
-                className="transform border-4 border-black bg-emerald-300/80 p-4 text-sm font-semibold shadow-[8px_8px_0px_rgba(0,0,0,1)] transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] active:translate-y-1 active:shadow-[2px_2px_0px_rgba(0,0,0,1)] dark:border-white dark:shadow-[8px_8px_0px_rgba(255,255,255,1)] dark:hover:shadow-[4px_4px_0px_rgba(255,255,255,1)] dark:active:shadow-[2px_2px_0px_rgba(255,255,255,1)] md:text-lg"
+                className="transform rounded-md border-4 border-foreground bg-emerald-300/90 p-4 text-sm font-semibold shadow-card transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] active:translate-y-1 active:shadow-[2px_2px_0px_rgba(0,0,0,1)] dark:text-background dark:active:shadow-[2px_2px_0px_rgba(255,255,255,1)] md:text-lg"
               >
                 {course}
               </div>
@@ -174,7 +173,7 @@ export default function Tkj() {
         <Button
           asChild
           size="lg"
-          className="hover:shadow-button border-4 border-black dark:border-white"
+          className="hover:shadow-button border-2 border-foreground transition"
         >
           <Link href="/guide/registration">Daftar Sekarang</Link>
         </Button>
