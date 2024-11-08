1adb5729e8d4a1b4b0c1be9ecf320b510a7af23f
paresiqbal <thecrowdel@gmail.com>
2024-11-08 20:46:53 +0700

    Refactor Pengumuman component for improved layout and styling, including updated heading presentation and enhanced tag visibility

--- app/(landing)/article/pengumuman/page.tsx
+++ app/(landing)/article/pengumuman/page.tsx
@@ -69,9 +69,9 @@ export default function Pengumuman() {
   if (error) return <p className="text-destructive">{error}</p>;
 
   return (
-    <div className="container mx-auto mb-8 flex max-w-[1200px] flex-col px-4 pt-6 md:px-0 md:pt-12">
-      <div className="space-y-2">
-        <h1 className="mb-2 text-3xl font-bold underline decoration-emerald-500 underline-offset-8 md:mb-6 md:text-5xl">
+    <div className="mx-auto mt-4 flex max-w-[1200px] flex-col pt-6 md:items-center md:justify-center md:pt-12">
+      <div className="space-y-2 pb-8">
+        <h1 className="scroll-m-20 text-balance text-4xl font-bold tracking-tight lg:text-5xl">
           Pengumuman
         </h1>
       </div>
@@ -86,7 +86,7 @@ export default function Pengumuman() {
               <span className="text-sm text-muted-foreground md:text-base">
                 {formatDate(item.created_at)}
               </span>
-              <span className="rounded-md bg-primary px-2 py-1 text-xs text-background md:text-sm">
+              <span className="hover:shadow-button cursor-pointer rounded-md border-2 border-foreground px-2 py-1 text-xs hover:bg-primary md:text-sm">
                 tags
               </span>
             </div>
@@ -94,20 +94,20 @@ export default function Pengumuman() {
             <div className="hidden md:block">
               <Separator
                 orientation="vertical"
-                className="h-full w-px bg-gray-400"
+                className="h-full w-px bg-foreground p-0.5"
               />
             </div>
 
             <div className="flex w-full flex-col md:w-3/4">
-              <div className="relative">
+              <div className="relative rounded-md border-2 border-foreground hover:shadow-card">
                 <Image
                   src={`${process.env.NEXT_PUBLIC_API_STORAGE}/${item.image}`}
                   width={500}
                   height={500}
                   alt={item.title}
-                  className="h-40 w-full rounded-md object-cover md:h-48"
+                  className="h-40 w-full object-cover md:h-48"
                 />
-                <span className="absolute bottom-2 right-2 rounded-md bg-primary bg-opacity-60 px-2 py-1 text-xs text-background md:text-sm">
+                <span className="absolute bottom-2 right-2 rounded-md border-2 border-foreground bg-primary/90 px-2 py-1 text-xs text-white">
                   pengumuman
                 </span>
               </div>
