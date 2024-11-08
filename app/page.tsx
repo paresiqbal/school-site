32b2a91cabf9c7a0363c50552d7a41edf1e29341
paresiqbal <thecrowdel@gmail.com>
2024-11-08 22:56:01 +0700

    Update Home page layout with new content sections, improved styling, and navigation links for enhanced user experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { CarouselPlugin } from "@/components/Carousel";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "SMK Negeri 1 Rejang Lebong",
@@ -14,32 +15,27 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <div className="min-h-screen items-center justify-items-center px-6 font-[family-name:var(--font-geist-sans)] md:px-10 lg:px-64">
         <CarouselPlugin />
         <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-          <div className="w-screen bg-amber-50 py-8">
+          <div className="w-screen bg-rose-50 py-8">
             <div className="mx-auto max-w-5xl px-4">
-              <h2 className="mb-6 text-center text-2xl font-bold text-gray-800">
-                SMK Negeri 1 Rejang Lebong
-              </h2>
-
-              <div className="flex flex-col items-center justify-around gap-8 md:flex-row">
-                {/* Left Section */}
-                <div className="text-center md:text-left">
-                  <p className="mb-4 text-lg text-gray-600">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing.
-                  </p>
-                  <div className="flex flex-col gap-4 md:flex-row">
-                    <Button className="rounded-lg bg-blue-600 px-6 py-2 text-white hover:bg-blue-700">
-                      Berkas PPDB
-                    </Button>
-                    <Button className="rounded-lg bg-green-600 px-6 py-2 text-white hover:bg-green-700">
-                      Brosur PPDB
-                    </Button>
-                  </div>
+              <div>
+                <h2 className="mb-4 text-2xl font-bold">
+                  SMK Negeri 1 Rejang Lebong
+                </h2>
+                <p className="text-muted-foreground">
+                  Membangun generasi muda cerdas dan berakhlak.
+                </p>
+              </div>
+              <div>
+                <div>
+                  <Link href="/guide/registration" className="px-6 py-2">
+                    Daftar
+                  </Link>
+                  <Button className="px-6 py-2">Brosur PPDB</Button>
                 </div>
 
-                {/* Right Section */}
                 <div className="grid grid-cols-3 gap-4 text-center">
                   <div className="flex flex-col items-center rounded-lg bg-white p-4 shadow-md">
                     <p className="text-3xl font-bold text-gray-800">A</p>
