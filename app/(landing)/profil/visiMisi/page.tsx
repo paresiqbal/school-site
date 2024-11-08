a77b4cbc6824fa8092b6a133d493ece08b70ae01
paresiqbal <thecrowdel@gmail.com>
2024-11-08 18:57:02 +0700

    Update heading styling in MissionVisionGoals component for improved readability

--- app/(landing)/profil/visiMisi/page.tsx
+++ app/(landing)/profil/visiMisi/page.tsx
@@ -26,7 +26,7 @@ export default function MissionVisionGoals() {
   return (
     <div className="mx-auto mt-4 flex max-w-[1200px] flex-col pt-6 md:items-center md:justify-center md:pt-12">
       <div className="space-y-2 pb-8 text-center">
-        <h1 className="mb-2 text-balance text-4xl font-bold md:mb-6 md:text-5xl">
+        <h1 className="scroll-m-20 text-balance text-4xl font-bold tracking-tight lg:text-5xl">
           SMK Negeri 1 Rejang Lebong
         </h1>
         <p className="text-md mb-12 text-muted-foreground md:text-lg">
