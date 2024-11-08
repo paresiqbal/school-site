a8a421ec0bddc339dcd66274dd245708c956ce09
paresiqbal <thecrowdel@gmail.com>
2024-11-08 23:25:02 +0700

    Refactor Home page layout to improve structure, enhance styling, and update content sections for better user engagement

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { CarouselPlugin } from "@/components/Carousel";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -15,42 +14,64 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen items-center justify-items-center px-6 font-[family-name:var(--font-geist-sans)] md:px-10 lg:px-64">
+      <div className="min-h-screen items-center justify-items-center px-6 font-[family-name:var(--font-geist-sans)]">
         <CarouselPlugin />
         <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
           <div className="w-screen bg-rose-50 py-8">
-            <div className="mx-auto max-w-5xl px-4">
+            <div className="mx-auto max-w-5xl">
               <div>
-                <h2 className="mb-4 text-2xl font-bold">
+                <h2 className="mb-4 text-3xl font-bold">
                   SMK Negeri 1 Rejang Lebong
                 </h2>
                 <p className="text-muted-foreground">
                   Membangun generasi muda cerdas dan berakhlak.
                 </p>
               </div>
-              <div>
-                <div>
-                  <Link href="/guide/registration" className="px-6 py-2">
+              <div className="flex items-center justify-between">
+                <div className="flex justify-between space-x-4">
+                  <Link
+                    href="/guide/registration"
+                    className="hover:shadow-button rounded-md border-2 border-foreground px-6 py-2 transition"
+                  >
                     Daftar
                   </Link>
-                  <Button className="px-6 py-2">Brosur PPDB</Button>
+                  <Link
+                    href="/guide/registration"
+                    className="hover:shadow-button rounded-md border-2 border-foreground bg-primary px-6 py-2 text-background transition"
+                  >
+                    Brosur PPDB
+                  </Link>
                 </div>
 
                 <div className="grid grid-cols-3 gap-4 text-center">
-                  <div className="flex flex-col items-center rounded-lg bg-white p-4 shadow-md">
-                    <p className="text-3xl font-bold text-gray-800">A</p>
-                    <p className="text-sm text-gray-500">Akreditasi</p>
+                  <div className="flex flex-col items-center rounded-md border-2 border-foreground bg-primary p-4 text-background transition hover:shadow-card">
+                    <p className="text-2xl font-bold">B</p>
+                    <p className="text-sm">Akreditasi</p>
                   </div>
-                  <div className="flex flex-col items-center rounded-lg bg-white p-4 shadow-md">
-                    <p className="text-3xl font-bold text-gray-800">1000+</p>
-                    <p className="text-sm text-gray-500">Jumlah siswa</p>
+                  <div className="flex flex-col items-center rounded-md border-2 border-foreground bg-primary p-4 text-background transition hover:shadow-card">
+                    <p className="text-2xl font-bold">200+</p>
+                    <p className="text-sm">Siswa</p>
                   </div>
-                  <div className="flex flex-col items-center rounded-lg bg-white p-4 shadow-md">
-                    <p className="text-3xl font-bold text-gray-800">7</p>
-                    <p className="text-sm text-gray-500">Jurusan</p>
+                  <div className="flex flex-col items-center rounded-md border-2 border-foreground bg-primary p-4 text-background transition hover:shadow-card">
+                    <p className="text-2xl font-bold">7</p>
+                    <p className="text-sm">Jurusan</p>
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="mx-auto my-6 flex max-w-5xl flex-col items-center justify-center gap-2">
+              <section className="flex justify-between space-x-20">
+                <p>Teknik Komputer & Jaringan</p>
+                <p>Teknik Bisnis Sepeda Motor</p>
+                <p>Teknik Kendaraan Ringan</p>
+                <p>Teknik Elektronika</p>
+              </section>
+              <section className="flex justify-between space-x-11">
+                <p>Teknik Desain Pemodelan</p>
+                <p>Teknik Instalasi Tenaga Listrik</p>
+                <p>Teknik Pengelasan</p>
+                <p>Teknik Mesin</p>
+              </section>
             </div>
           </div>
         </main>
