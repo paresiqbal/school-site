7d1fe014c5a2343bb7230769ef4a8fc3c0739368
paresiqbal <thecrowdel@gmail.com>
2024-11-09 17:33:33 +0700

    Add Sambutan component and refactor Home page layout for improved structure and styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { CarouselPlugin } from "@/components/Carousel";
+import Sambutan from "@/components/Sambutan";
 
 export const metadata: Metadata = {
   title: "SMK Negeri 1 Rejang Lebong",
@@ -15,10 +16,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen items-center justify-items-center px-6 font-[family-name:var(--font-geist-sans)]">
+      <div className="min-h-screen items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
         <CarouselPlugin />
-        <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-          <div className="flex w-screen items-center justify-center"></div>
+        <main className="row-start-2 flex w-full flex-col items-center justify-center gap-8 sm:items-start">
+          <Sambutan />
         </main>
         <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
       </div>
--- components/Sambutan.tsx
+++ components/Sambutan.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Sambutan() {
   return (
-    <div className="w-screen bg-rose-50 py-8 dark:bg-accent">
+    <div className="w-full overflow-hidden bg-rose-50 py-8 dark:bg-accent">
       <div className="mx-auto max-w-5xl px-4">
         <div className="mb-8 text-center md:text-left">
           <h2 className="mb-2 text-2xl font-bold md:text-3xl">
