847cd732e608d07f4a339f1f922ed7eba083ce7d
paresiqbal <thecrowdel@gmail.com>
2024-11-11 19:53:24 +0700

    Update GalleryPlugin component: adjust layout for better responsiveness and improve text styling

--- components/GalleryPlugin.tsx
+++ components/GalleryPlugin.tsx
@@ -7,23 +7,23 @@ export default function GalleryPlugin() {
       <h2 className="mb-6 text-sm uppercase tracking-wide text-gray-500">
         GALLERY
       </h2>
-      <div className="flex items-center justify-between gap-6">
-        <div className="w-3/4">
+      <div className="flex flex-col items-center justify-between gap-6 lg:flex-row lg:items-start">
+        <div className="w-full lg:w-3/4">
           <Image
             src="/assets/gallery.png"
             width={500}
             height={500}
             quality={100}
             alt="gallery"
-            className="h-72 w-full rounded-md object-cover"
+            className="h-64 w-full rounded-md object-cover lg:h-72"
           />
         </div>
-        <div className="w-1/4">
-          <p className="mb-4 text-3xl font-bold leading-tight">
+        <div className="w-full text-center lg:w-1/4 lg:text-left">
+          <p className="mb-4 text-2xl font-bold leading-tight lg:text-3xl">
             Dokumentasi Foto & Video SMK Negeri 1 Rejang Lebong
           </p>
           <Link href="/gallery">
-            <button className="hover:shadow-button rounded-md border-2 border-foreground bg-primary/90 p-2 text-background hover:bg-primary">
+            <button className="hover:shadow-button mt-4 rounded-md border-2 border-foreground p-2 hover:bg-primary">
               Lihat Lebih Banyak
             </button>
           </Link>
