667191614944b45c84fca99852fabcae13d0d609
paresiqbal <thecrowdel@gmail.com>
2024-11-21 21:53:00 +0700

    Tidak jadi

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
       <div className="min-h-screen items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
         <CarouselPlugin />
         <main className="row-start-2 flex w-full flex-col items-center justify-center sm:items-start">
-          <h1>Test doang</h1>
           <SambutanPlugin />
           <KepsekPlugin />
           <InfoPlugin />
