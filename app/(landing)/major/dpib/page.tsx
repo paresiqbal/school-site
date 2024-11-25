2a82d2199793fed74376c170558dfa9bb7fa2e7b
paresiqbal <thecrowdel@gmail.com>
2024-11-25 21:31:53 +0700

    Enhance Tkj component: update background color and text size for course and career cards

--- app/(landing)/major/dpib/page.tsx
+++ app/(landing)/major/dpib/page.tsx
@@ -148,7 +148,7 @@ export default function Tkj() {
             ].map((course) => (
               <div
                 key={course}
-                className="transform rounded-md border-2 border-foreground bg-emerald-300/90 p-4 text-sm font-semibold shadow-card transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] active:translate-y-1 active:shadow-[2px_2px_0px_rgba(0,0,0,1)] dark:text-background dark:active:shadow-[2px_2px_0px_rgba(255,255,255,1)] md:text-lg"
+                className="transform rounded-md border-2 border-foreground bg-emerald-400 p-4 text-sm font-semibold shadow-card transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-300 hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] active:translate-y-1 active:shadow-[2px_2px_0px_rgba(0,0,0,1)] dark:text-background dark:active:shadow-[2px_2px_0px_rgba(255,255,255,1)] md:text-base"
               >
                 {course}
               </div>
@@ -173,7 +173,7 @@ export default function Tkj() {
             ].map((career) => (
               <div
                 key={career}
-                className="transform rounded-md border-2 border-foreground bg-emerald-300/90 p-4 text-sm font-semibold shadow-card transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-400 hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] active:translate-y-1 active:shadow-[2px_2px_0px_rgba(0,0,0,1)] dark:text-background dark:active:shadow-[2px_2px_0px_rgba(255,255,255,1)] md:text-lg"
+                className="transform rounded-md border-2 border-foreground bg-emerald-400 p-4 text-sm font-semibold shadow-card transition-all duration-150 ease-in-out hover:translate-y-1 hover:bg-emerald-300 hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] active:translate-y-1 active:shadow-[2px_2px_0px_rgba(0,0,0,1)] dark:text-background dark:active:shadow-[2px_2px_0px_rgba(255,255,255,1)] md:text-base"
               >
                 {career}
               </div>
